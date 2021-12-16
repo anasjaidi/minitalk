ce5a85d8eb5601f6068a69c76b2a9289a0aa12c0
Anas Jaidi <ajaidi@e1r9p7.1337.ma>
2021-12-16 19:56:33 +0100

    done

--- convert.c
+++ convert.c
@@ -1,54 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   convert.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ajaidi <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/29 19:23:13 by ajaidi            #+#    #+#             */
+/*   Updated: 2021/12/16 19:23:36 by ajaidi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
 #include <signal.h>
 #include <stdlib.h>
 #include <unistd.h>
+
 void	send_signal(char *str, int pid)
 {
-	int i = -1;
+	int	i;
+
+	i = -1;
 	while (str[++i])
 	{
 		if (str[i] == 48)
-		{
 			kill(pid, SIGUSR1);
-			printf("0");
-		}
-		else{
+		else
 			kill(pid, SIGUSR2);
-			printf("1");
-		}
 		usleep(500);
 	}
 }
 
-char	*convert(char	c)
+char	*convert(char c)
 {
-	int x = (int)c;
-	char	*bin = malloc(9);
-	int i = 7;
-	while(x)
+	int		x;
+	char	*bin;
+	int		i;
+
+	i = 8;
+	x = c;
+	bin = malloc(10);
+	bin[9] = 0;
+	if (c < 0)
+		bin[0] = '1';
+	else
+		bin[0] = '0';
+	while (x)
 	{
 		bin[i] = (x % 2) + 48;
 		x /= 2;
 		i--;
 	}
-	while (i >= 0)
+	while (i > 0)
 	{
 		bin[i] = 48;
 		i--;
 	}
-	return bin;
+	return (bin);
 }
 
-int main (int argc, char *argv[])
+int	main(int argc, char *argv[])
 {
-	int i = -1;
+	int		i;
 	char	*str;
-	while(argv[2][++i])
+
+	i = -1;
+	while (argv[2][++i])
 	{
 		str = convert(argv[2][i]);
-		send_signal(str , atoi(argv[1]));
-		printf("%d\n", atoi(argv[1]));
-		//printf("%s\n", str);
+		send_signal(str, atoi(argv[1]));
 	}
-	return 0;
+	return (0);
 }
--- main.c
+++ main.c
@@ -1,35 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ajaidi <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/16 19:52:14 by ajaidi            #+#    #+#             */
+/*   Updated: 2021/12/16 19:52:21 by ajaidi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
 #include <signal.h>
 #include <unistd.h>
 #include <stdlib.h>
 
-
 void	handler(int prb)
 {
-	static int i;
+	static int	i;
+	static char	x;
+	static int	sign;
+
 	prb -= SIGUSR1;
-	static char x;
+	if (i == 0 && prb == 1)
+	{
+		sign = -1;
+		i++;
+		return ;
+	}
+	else if (i == 0 && prb == 0)
+		sign = 1;
 	x = x << 1 | prb;
 	i++;
-	if (i == 8)
+	if (i == 9)
 	{
-		write(1,&x,1);
+		x *= sign;
+		write(1, &x, 1);
 		i = 0;
 		x = 0;
+		sign = 1;
 	}
 }
 
-
-int main()
+int	main(void)
 {
-	printf("pid	:	%d\n",  getpid());
+	printf("pid	:	%d\n", getpid());
 	signal(SIGUSR2, handler);
 	signal(SIGUSR1, handler);
 	while (1)
-	{
 		sleep(1);
-	}
 }
-
-
-
